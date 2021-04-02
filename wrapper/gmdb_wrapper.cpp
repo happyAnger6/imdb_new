85f9af277f664e13eabe34f684f99304c0eb7bda
证件照 <happyAnger6@163.com>
2021-04-02 18:01:04 +0800

    Update gmdb_wrapper.cpp

--- wrapper/gmdb_wrapper.cpp
+++ wrapper/gmdb_wrapper.cpp
@@ -13,9 +13,12 @@ int db_update_obj(db_object obj, db_key_info_t* key_info, void* key_data)
     return ((TableBase *)obj)->save();
 }
 
-int db_read_obj(db_object obj, db_key_info_t* key_info, void* key_data)
+extern int db_read_obj_with_buf(db_object obj, 
+        db_key_info_t* key_info, void* key_data, void* buf, uint32_t buf_len)
 {
-    return 0;
+    TableBase *pTableBase = ((TableBase *)obj);
+    TableIndex index = pTableBase->create_index(key_info->index_id, key_data);
+    pTableBase->read_obj(index, buf, buf_len);
 }
 
 int db_set_key_data(db_object obj, db_key_info_t* key_info, void* key_data)
