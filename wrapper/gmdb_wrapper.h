9d350502c5f42942828f84dfdd5e87f7ff087da1
证件照 <happyAnger6@163.com>
2021-04-02 17:09:58 +0800

    Update gmdb_wrapper.h

--- wrapper/gmdb_wrapper.h
+++ wrapper/gmdb_wrapper.h
@@ -10,5 +10,10 @@ using db_key_info_t = int;
 
 extern int db_write_obj(db_object obj);
 extern int db_update_obj(db_object obj, db_key_info_t* key_info, void* key_data);
-
+extern int db_create_child_node(db_object object,
+    db_child_iterator parent_iterator,
+    const char *child_node_name,
+    db_key_info_t* key_info,
+    void* key_data,
+    db_child_iterator *child_iterator);
 #endif
